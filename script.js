6c7cc4a73645f1485118797d288cf051625e12ee
daratehexplorer <darinka.lipovac@gmail.com>
2023-10-31 12:57:03 +0000

    Edit styles.css and script.js to make number of squares editable

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const body = document.querySelector("body");
+
+const header = document.createElement('div');
+header.classList.add('header')
+    const title = document.createElement('div');
+    title.classList.add('title')
+    title.textContent = 'ETCH-A-SKETCH';
+        header.appendChild(title);
+    const textBox = document.createElement('div');
+    textBox.classList.add('textBox')
+        header.appendChild(textBox);
+body.appendChild(header);
+
 const container = document.createElement('div');
 container.classList.add('container');
 body.appendChild(container);
-    
 
+const content = document.createElement('div')
+content.classList.add('content');
+container.appendChild(content);
 
-for (i = 0; i < 256; i++) {
+for (i = 0; i < 400; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
-    container.appendChild(square);
+    content.appendChild(square);
 };
 
